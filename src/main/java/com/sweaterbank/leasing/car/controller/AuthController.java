796c584ef4dc6a0e6773713087c44625f0efafc4
PijusVose <pijus.vose@gmail.com>
2024-04-12 13:53:15 +0300

    feat: sign up endpoint; feat: added jakarta validation dependency

--- src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
+++ src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
@@ -4,24 +4,31 @@ import com.sweaterbank.leasing.car.controller.dto.SignInRequest;
 import com.sweaterbank.leasing.car.controller.dto.SignInResponse;
 import com.sweaterbank.leasing.car.controller.dto.SignOutRequest;
 import com.sweaterbank.leasing.car.controller.dto.SignOutResponse;
+import com.sweaterbank.leasing.car.controller.dto.SignUpRequest;
+import com.sweaterbank.leasing.car.controller.dto.SignUpResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController
 {
-    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    @PostMapping("signin")
     public ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest requestData) {
         return ResponseEntity.ok(new SignInResponse("jwt-token"));
     }
 
-    @RequestMapping()
+    @PostMapping("signout")
     public ResponseEntity<SignOutResponse> signOut(@Valid @RequestBody SignOutRequest requestData) {
-        return ResponseEntity.ok(new SignOutResponse("username"));
+        return ResponseEntity.ok(new SignOutResponse(requestData.username()));
+    }
+
+    @PostMapping("signup")
+    public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest requestData) {
+        return ResponseEntity.ok(new SignUpResponse("jwt-success-token"));
     }
 }
