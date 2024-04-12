a87ba2cbef6eb9293d38e7c84cfebb1f1de3bc68
PijusVose <pijus.vose@gmail.com>
2024-04-12 14:56:25 +0300

    chore: removed todo comment

--- src/main/java/com/sweaterbank/leasing/car/controller/dto/SignUpRequest.java
+++ src/main/java/com/sweaterbank/leasing/car/controller/dto/SignUpRequest.java
@@ -2,5 +2,4 @@ package com.sweaterbank.leasing.car.controller.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-// TODO: add user role
 public record SignUpRequest (@NotNull(message = "Username may not be empty") String username, @NotNull(message = "Password may not be empty") String password) { }
