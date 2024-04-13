03a0524edcc2ed2ea079c8c669df422619d60e2b
Jamtit <majauskas.titas@gmail.com>
2024-04-13 16:54:07 +0300

    feat: created JwtService class; chore: added dependencies to manipulate JWTs

--- src/main/java/com/sweaterbank/leasing/car/config/JwtAuthenticationFilter.java
+++ src/main/java/com/sweaterbank/leasing/car/config/JwtAuthenticationFilter.java
@@ -1,9 +1,11 @@
 package com.sweaterbank.leasing.car.config;
 
+import com.sweaterbank.leasing.car.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -11,18 +13,22 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
+        final String userEmail; //todo: extract userEmail from JWTTOken
 
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response); //pushes the request and response down the chain
             return;
         }
-    }
+
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtService.extractEmail(jwtToken);
 }
---
+++ src/main/java/com/sweaterbank/leasing/car/services/JwtService.java
@@ -0,0 +1,10 @@
+package com.sweaterbank.leasing.car.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractEmail(String jwt){
+      return null;
+    }
+}
