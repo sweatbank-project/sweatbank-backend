f717a55c73a636c1410f16e54c679c67de4afab2
Jamtit <majauskas.titas@gmail.com>
2024-04-13 18:29:10 +0300

    feat: added a function to generate our token

--- src/main/java/com/sweaterbank/leasing/car/config/JwtAuthenticationFilter.java
+++ src/main/java/com/sweaterbank/leasing/car/config/JwtAuthenticationFilter.java
@@ -22,7 +22,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
-        final String userEmail; //todo: extract userEmail from JWTTOken
+        final String userEmail;
 
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response); //pushes the request and response down the chain
@@ -31,4 +31,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         jwtToken = authHeader.substring(7);
         userEmail = jwtService.extractEmail(jwtToken);
+        //todo: extract userEmail from JWTToken
 }
--- src/main/java/com/sweaterbank/leasing/car/services/JwtService.java
+++ src/main/java/com/sweaterbank/leasing/car/services/JwtService.java
@@ -2,16 +2,22 @@ package com.sweaterbank.leasing.car.services;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "76D9E76ADCB9D8FCB4EB3FD18C44E";
+    private static final int EXPIRATION_TIME_IN_MS = 60000;
     public String extractEmail(String token){
       return extractClaim(token, Claims::getSubject);
     }
@@ -19,6 +25,23 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MS))
+                .signWith(getSigningKey())
+                .compact();
     }
 
     private Claims extractAllClaims(String token){
