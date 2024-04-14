513e9fc5e66b27b59fb6b685f594611e570a1325
Jamtit <majauskas.titas@gmail.com>
2024-04-14 14:41:51 +0300

    fix: removed redudant creation of the same users table and dropping it for a better version, now it creates the good version at the start

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,5 +1,12 @@
-CREATE TABLE users(
-    id VARCHAR PRIMARY KEY ,
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id VARCHAR PRIMARY KEY,
+    personal_data_id VARCHAR NOT NULL REFERENCES PersonalData(id),
     username VARCHAR NOT NULL,
-    role roles NOT NULL
-)
+    password VARCHAR NOT NULL,
+    role VARCHAR NOT NULL,
+    account_expiration_date TIMESTAMP,
+    account_locked BOOLEAN,
+    enabled BOOLEAN
+);
