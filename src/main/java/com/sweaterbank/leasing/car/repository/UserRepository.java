2c15410403ca00675f27562433c6da004e86f481
PijusVose <pijus.vose@gmail.com>
2024-04-15 21:54:51 +0300

    refactor: queries constants

--- src/main/java/com/sweaterbank/leasing/car/repository/UserRepository.java
+++ src/main/java/com/sweaterbank/leasing/car/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.sweaterbank.leasing.car.repository;
 import com.sweaterbank.leasing.car.controller.dto.SignUpRequest;
 import com.sweaterbank.leasing.car.model.Roles;
 import com.sweaterbank.leasing.car.model.User;
+import com.sweaterbank.leasing.car.repository.contants.Queries;
 import com.sweaterbank.leasing.car.repository.mappers.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -27,9 +28,6 @@ public class UserRepository implements UserRepositoryInterface{
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
     private PasswordEncoder passwordEncoder;
 
     public UserRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate)
@@ -39,35 +37,27 @@ public class UserRepository implements UserRepositoryInterface{
 
     @Override
     public Optional<User> selectUserByEmail(String username) {
-        String query = """
-                    SELECT * from users
-                    WHERE username = :username
-                """;
         SqlParameterSource params = new MapSqlParameterSource()
                 .addValue("username", username);
 
-        return namedParameterJdbcTemplate.query(query, params, new UserMapper())
+        return namedParameterJdbcTemplate.query(Queries.SELECT_BY_EMAIL_QUERY, params, new UserMapper())
                 .stream()
                 .findFirst();
     }
 
     public void saveUser(SignUpRequest request) {
-        String query = """
-                    INSERT INTO users(id, username, personal_data_id, password, role, account_locked, enabled)
-                    VALUES (:id, :username, :personal_data_id, :password, :role, :account_locked, :enabled);
-                """;
-
         String generatedUUID = UUID.randomUUID().toString();
+        String encodedPassword = passwordEncoder.encode(request.password());
 
         MapSqlParameterSource params = new MapSqlParameterSource()
                 .addValue("id", generatedUUID)
                 .addValue("username", request.username())
                 .addValue("personal_data_id", null)
-                .addValue("password", passwordEncoder.encode(request.password()))
+                .addValue("password", encodedPassword)
                 .addValue("role", Roles.USER.toString())
                 .addValue("account_locked", false)
                 .addValue("enabled", true);
 
-        namedParameterJdbcTemplate.update(query, params);
+        namedParameterJdbcTemplate.update(Queries.SAVE_USER_QUERY, params);
     }
 }
---
+++ src/main/java/com/sweaterbank/leasing/car/repository/contants/Queries.java
@@ -0,0 +1,8 @@
+package com.sweaterbank.leasing.car.repository.contants;
+
+public class Queries
+{
+    public static final String SAVE_USER_QUERY = "INSERT INTO users(id, username, personal_data_id, password, role, account_locked, enabled)\n" +
+            "VALUES (:id, :username, :personal_data_id, :password, :role, :account_locked, :enabled);";
+    public static final String SELECT_BY_EMAIL_QUERY = "SELECT * from users\nWHERE username = :username";
+}
