4a90434cd48c0c6322c9b61d6be0d2488631682d
Jamtit <majauskas.titas@gmail.com>
2024-04-15 13:24:57 +0300

    fix: fixed flyway migration validation error

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,4 +1,15 @@
-DROP TABLE IF EXISTS users;
+CREATE TABLE PersonalData (
+      id VARCHAR PRIMARY KEY,
+      name VARCHAR NOT NULL,
+      surname VARCHAR NOT NULL,
+      birthdate DATE NOT NULL,
+      identityNumber VARCHAR NOT NULL UNIQUE,
+      salary DECIMAL(10, 2) NOT NULL,
+      education VARCHAR NOT NULL,
+      positionHeld VARCHAR NOT NULL,
+      jobTitle VARCHAR
+);
+
 
 CREATE TABLE users (
     id VARCHAR PRIMARY KEY,
@@ -9,4 +20,24 @@ CREATE TABLE users (
     account_expiration_date TIMESTAMP,
     account_locked BOOLEAN,
     enabled BOOLEAN
+);
+
+
+CREATE TABLE Car (
+     id VARCHAR PRIMARY KEY,
+     make VARCHAR NOT NULL,
+     model VARCHAR NOT NULL,
+     year INTEGER NOT NULL,
+     cost DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE Leasing (
+     id VARCHAR PRIMARY KEY,
+     user_id VARCHAR NOT NULL REFERENCES users(id),
+     car_id VARCHAR NOT NULL REFERENCES Car(id),
+     startDate DATE NOT NULL,
+     endDate DATE NOT NULL,
+     monthlyPayment DECIMAL(10, 2) NOT NULL,
+     deposit DECIMAL(10, 2) NOT NULL,
+     status VARCHAR NOT NULL
 );
