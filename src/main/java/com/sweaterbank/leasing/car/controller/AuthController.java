d99963beadc5481768339aca0105c1062a6e9a4f
PijusVose <pijus.vose@gmail.com>
2024-04-16 15:35:16 +0300

    feat: added cross origin handling to AuthController

--- src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
+++ src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
@@ -15,11 +15,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController
