cd7b283a6ca1b6c955ddb6a70980a78c27807a54
Jamtit <majauskas.titas@gmail.com>
2024-04-16 16:08:50 +0300

    fix: changed cross origin value to represent direct localhost url that frontend uses

--- src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
+++ src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController
