147e3c535400d80226dfa82b8646036730dc7814
PijusVose <pijus.vose@gmail.com>
2024-04-16 18:50:12 +0300

    refactor: removed unused libraries in SecurityConfig

--- src/main/java/com/sweaterbank/leasing/car/config/SecurityConfig.java
+++ src/main/java/com/sweaterbank/leasing/car/config/SecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
