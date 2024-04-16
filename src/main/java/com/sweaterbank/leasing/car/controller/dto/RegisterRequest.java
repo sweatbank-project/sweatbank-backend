4af007d5732e580ec1c1732a293e11abd033a372
Jamtit <majauskas.titas@gmail.com>
2024-04-16 22:28:30 +0300

    refactor: impoved RegisterRequest DTO's structure to portray the needed field; feat: added basic validation annotations

--- src/main/java/com/sweaterbank/leasing/car/controller/dto/RegisterRequest.java
+++ src/main/java/com/sweaterbank/leasing/car/controller/dto/RegisterRequest.java
@@ -1,16 +1,49 @@
 package com.sweaterbank.leasing.car.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.sql.Date;
 
-public record RegisterRequest(@NotNull(message = "Username may not be empty") String username,
-                              @NotNull(message = "Password may not be empty") String password,
-                              @NotNull(message = "First name may not be empty") String firstName,
-                              @NotNull(message = "Last name may not be empty") String lastName,
-                              @NotNull(message = "Birthdate may not be empty") Date birthDate,
-                              @NotNull(message = "Salary may not be empty") BigDecimal salary,
-                              @NotNull(message = "Education may not be empty") String education,
-                              @NotNull(message = "Position held may not be empty") String positionHeld,
-                              @NotNull(message = "Job title may not be empty") String jobTitle) { }
+public record RegisterRequest(
+        @NotEmpty
+        @NotBlank
+        @NotNull(message = "Username may not be null")
+        @Email
+        String username,
+        @NotEmpty
+        @NotBlank
+        @NotNull(message = "Field phone number may not be null")
+        String phoneNumber,
+        @Size(min = 9, max = 9)
+        @NotEmpty
+        @NotBlank
+        @NotNull(message = "Personal ID may not be null")
+        String personalId,
+        @NotEmpty
+        @NotBlank
+        @Size(min = 6, message = "The password must be at least 6 characters long")
+        @NotNull(message = "Password may not be null")
+        String password,
+        @NotEmpty
+        @NotBlank
+        @NotNull(message = "First name may not be null") String firstName,
+        @NotEmpty
+        @NotBlank
+        @NotNull(message = "Last name may not be null") String lastName,
+        @NotEmpty
+        @NotBlank
+        @PastOrPresent
+        @NotNull(message = "Birthdate may not be null")
+        Date birthDate,
+        @NotEmpty
+        @NotBlank
+        @NotNull(message = "Address may not be null")
+        String address
+) { }
