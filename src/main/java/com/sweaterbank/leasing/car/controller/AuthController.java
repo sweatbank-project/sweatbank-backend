267c780106f03640dd44b935e683149cc93acd0a
Jamtit <majauskas.titas@gmail.com>
2024-04-16 16:24:59 +0300

    fix: changing @CrossOrigin annotation - adding another annotation at login method level

--- src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
+++ src/main/java/com/sweaterbank/leasing/car/controller/AuthController.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/auth")
 public class AuthController
 {
