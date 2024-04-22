a940ef98968122b45da2f6aae0a3d661944b1a15
PijusVose <pijus.vose@gmail.com>
2024-04-22 18:48:31 +0300

    refactor: changed leasing and obligation table decimal points

---
+++ src/main/resources/db/migration/V6__update_leasing_table.sql
@@ -0,0 +1,4 @@
+ALTER TABLE Leasing ALTER COLUMN car_cost TYPE DECIMAL(10, 2);
+ALTER TABLE Leasing ALTER COLUMN monthly_income_after_taxes TYPE DECIMAL(10, 2);
+ALTER TABLE Obligation ALTER COLUMN outstanding_debt TYPE DECIMAL(10, 2);
+ALTER TABLE Obligation ALTER COLUMN monthly_payment TYPE DECIMAL(10, 2);
