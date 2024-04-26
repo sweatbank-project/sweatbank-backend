f3e30232ab92a84ac752a6ca5fb76cfe0ceaaa73
Jamtit <majauskas.titas@gmail.com>
2024-04-26 14:48:50 +0300

    feat: relocated JWT variables into env variables

--- src/main/java/com/sweaterbank/leasing/car/config/JwtAuthenticationFilter.java
+++ src/main/java/com/sweaterbank/leasing/car/config/JwtAuthenticationFilter.java
@@ -1,16 +1,13 @@
 package com.sweaterbank.leasing.car.config;
 
 import com.sweaterbank.leasing.car.services.JwtService;
-import com.sweaterbank.leasing.car.services.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -18,16 +15,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtService jwtService;
-    @Autowired
-    private UserDetailsService userDetailsService;
 
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService){
+        this.jwtService=jwtService;
+        this.userDetailsService=userDetailsService;
+    }
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
