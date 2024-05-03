a5eadf16f133aaeae4e4f72479dd1f74639f5bc9
Eimantas <eimantas.dziovenas@gmail.com>
2024-05-04 02:20:40 +0300

    fix: return more data in solvency calculation response

--- src/main/java/com/sweaterbank/leasing/car/controller/dto/responses/CalculationResponse.java
+++ src/main/java/com/sweaterbank/leasing/car/controller/dto/responses/CalculationResponse.java
@@ -3,7 +3,12 @@ package com.sweaterbank.leasing.car.controller.dto.responses;
 import java.math.BigDecimal;
 
 public record CalculationResponse(
-        BigDecimal loanServiceRate,
-        BigDecimal totalMonthlyPayments
+        BigDecimal netIncome,
+        BigDecimal currentMonthlyPayment,
+        BigDecimal otherMonthlyPayments,
+        BigDecimal newMonthlyPayment,
+        BigDecimal householdExpenditure,
+        BigDecimal monthlyLivingExpenses,
+        BigDecimal loanServiceRate
 ) {
 }
